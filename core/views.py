f14edea8288e8fbc99fdd6d6fcce985357fb7993
Give_Kenzo <egorsh2405@gmail.com>
2024-06-12 19:47:40 +0800

    hot fix

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.serializers import (CreateUserSerializer, LoginSerializer,
-                              ProfileSerializer, UpdatePasswordSerializer)
+                            ProfileSerializer, UpdatePasswordSerializer)
 
 from .models import User
 
