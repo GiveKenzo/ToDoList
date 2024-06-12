4a0e11a0d085185ac4ea6f5c9ed6a66274762993
Give_Kenzo <egorsh2405@gmail.com>
2024-06-12 19:47:10 +0800

    hot fix

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import (AuthenticationFailed, NotAuthenticated,
+                                       ValidationError)
 
 from core.models import User
 
@@ -39,3 +41,48 @@ class CreateUserSerializer(serializers.ModelSerializer):
         del validated_data['password_repeat']
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required = True)
+    password = PasswordField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        
+    def create(self, validated_data: dict):
+        if not (user := authenticate(
+                username = validated_data['username'],
+                password = validated_data['password']
+        )):
+            raise AuthenticationFailed
+        return user
+
+# Все поля которые у нас есть, кроме пароля мы будем принимать и отдавать
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+# Реализация смены пароля
+class UpdatePasswordSerializer(serializers.Serializer):
+    user = serializers.HiddenField(default = serializers.CurrentUserDefault())
+    old_password = PasswordField(required = True)
+    new_password = PasswordField(required = True)
+    
+    # Валидация старого пароля
+    def validate(self, attrs: dict):
+        if not(user := attrs['user']):
+            raise NotAuthenticated
+        if not user.check_password(attrs['ola_password']):
+            raise ValidationError({'old password': 'field is incorrect'})
+        return attrs
+
+    def create(self, validated_data: dict):
+        return NotImplementedError
+    
+    # Новый пароль
+    def update(self, instance, validated_data: dict):
+        instance.password = make_password(validated_data['new_passwrod'])
+        instance.save(update_fields = ('password', ))
+        return instance
--- core/views.py
+++ core/views.py
@@ -1,7 +1,43 @@
-from rest_framework import generics
+from django.contrib.auth import login, logout
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer
+from core.serializers import (CreateUserSerializer, LoginSerializer,
+                              ProfileSerializer, UpdatePasswordSerializer)
+
+from .models import User
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
+    
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        login(request = request, user = serializer.save())
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+# Нам нужно получать, обновлять и удалять
+class ProfileView(generics.RetrieveDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]#Аутентификация
+    
+    def get_object(self): #Возвращает пользователя, потому что queryset лежит user
+        return self.request.user
+    
+    def delete(self, request, *args, **kwargs):
+        # В данном случае logout просто почистит куки
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+    
+    def get_object(self):
+        return self.request.user
